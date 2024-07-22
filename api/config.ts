35367783f71b2d258525611ba7ce191efb85d827
marcomaza92 <marcoagustinmaza@gmail.com>
2024-07-21 22:06:48 -0300

    feat: update API URL to match production deployment

--- api/config.ts
+++ api/config.ts
@@ -1,1 +1,4 @@
-export const API_URL = `http://localhost:8000/api/`;
+export const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://titanstar-legends-rune-calculator-api.vercel.app/api"
+    : "http://localhost:8000/api";
